1bb39c173312702f9d9d970c1fe924f4dbcd363e
Kangjik Kim <rkqqkb@gmail.com>
2023-02-18 19:10:34 +0900

    슬라이드 애니메이션 추가

--- static/script.js
+++ static/script.js
@@ -11,66 +11,84 @@ var currentIndex = 0;
 
 // 다음 단계로 이동하는 함수
 function next() {
-  current_fs = fieldsets[currentIndex];
-  next_fs = fieldsets[currentIndex + 1];
+	current_fs = fieldsets[currentIndex];
+	next_fs = fieldsets[currentIndex + 1];
 
-  currentProgress = progressList[currentIndex];
-  nextProgress = progressList[currentIndex + 1];
+	currentProgress = progressList[currentIndex];
+	nextProgress = progressList[currentIndex + 1];
 
-  // form 유효성 검사
-  if (currentIndex === 0 && !validateStep1()) {
-    return;
-  }
+	// form 유효성 검사
+	if (currentIndex === 0 && !validateStep1()) {
+		return;
+	}
 
-  // 필드셋 전환
-  current_fs.style.display = "none";
-  next_fs.style.display = "block";
+	// 필드셋 전환
+	// 현재 필드셋 및 다음 필드셋에 애니메이션 클래스 추가
+	current_fs.classList.add('slideOut');
+	next_fs.classList.add('slideIn');
 
-  // progressbar 업데이트
-  nextProgress.classList.add("active");
+	// 0.5초 후 애니메이션 클래스 제거
+	setTimeout(() => {
+		current_fs.style.display = 'none';
+		next_fs.style.display = 'block';
+		current_fs.classList.remove('slideOut');
+		next_fs.classList.remove('slideIn');
+	}, 500);
 
-  // 현재 위치 업데이트
-  currentIndex++;
+	// progressbar 업데이트
+	nextProgress.classList.add("active");
+
+	// 현재 위치 업데이트
+	currentIndex++;
 }
 
 // 이전 단계로 이동하는 함수
 function previous() {
-  current_fs = fieldsets[currentIndex];
-  previous_fs = fieldsets[currentIndex - 1];
+	current_fs = fieldsets[currentIndex];
+	previous_fs = fieldsets[currentIndex - 1];
 
-  currentProgress = progressList[currentIndex];
-  previousProgress = progressList[currentIndex - 1];
+	currentProgress = progressList[currentIndex];
+	previousProgress = progressList[currentIndex - 1];
 
-  // 필드셋 전환
-  current_fs.style.display = "none";
-  previous_fs.style.display = "block";
+	// 필드셋 전환
+	// 현재 필드셋 및 이전 필드셋에 애니메이션 클래스 추가
+	current_fs.classList.add('slideOut');
+	previous_fs.classList.add('slideIn');
 
-  // progressbar 업데이트
-  currentProgress.classList.remove("active");
-  previousProgress.classList.add("active");
+	// 0.5초 후 애니메이션 클래스 제거
+	setTimeout(() => {
+		current_fs.style.display = 'none';
+		previous_fs.style.display = 'block';
+		current_fs.classList.remove('slideOut');
+		previous_fs.classList.remove('slideIn');
+	}, 500);
 
-  // 현재 위치 업데이트
-  currentIndex--;
+	// progressbar 업데이트
+	currentProgress.classList.remove("active");
+	previousProgress.classList.add("active");
+
+	// 현재 위치 업데이트
+	currentIndex--;
 }
 
 // STEP1 폼 유효성 검사 함수
 function validateStep1() {
-  var feature1 = form.querySelector('input[name="feature1"]:checked');
-  if (!feature1) {
-    alert("성별을 선택해주세요.");
-    return false;
-  }
-  return true;
+	var feature1 = form.querySelector('input[name="feature1"]:checked');
+	if (!feature1) {
+		alert("성별을 선택해주세요.");
+		return false;
+	}
+	return true;
 }
 
 // next 버튼 클릭 시 이벤트 리스너
 var nextButtons = document.querySelectorAll(".next");
 for (var i = 0; i < nextButtons.length; i++) {
-  nextButtons[i].addEventListener("click", next);
+	nextButtons[i].addEventListener("click", next);
 }
 
 // previous 버튼 클릭 시 이벤트 리스너
 var previousButtons = document.querySelectorAll(".previous");
 for (var i = 0; i < previousButtons.length; i++) {
-  previousButtons[i].addEventListener("click", previous);
+	previousButtons[i].addEventListener("click", previous);
 }
